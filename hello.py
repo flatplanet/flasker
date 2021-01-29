0631d9f9171f7bfc9c084d33b021173f5248710e
John Elder <johne4196@gmail.com>
2021-01-29 08:01:41 -0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+
+#def index():
+#	return "<h1>Hello World!</h1>"
+
+# FILTERS!!!
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
+
+
+def index():
+	first_name = "John"
+	stuff = "This is bold text"
+
+	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+	return render_template("index.html", 
+		first_name=first_name,
+		stuff=stuff,
+		favorite_pizza = favorite_pizza)
+
+# localhost:5000/user/John
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", user_name=name)
+
+# Create Custom Error Pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500

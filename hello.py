7ceeb365989ef8542155252ff57e63bfe39fa510
John Elder <johne4196@gmail.com>
2021-03-18 10:17:06 -0700

    update user records

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -19,6 +19,11 @@ app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
 # Initialize The Database
 db = SQLAlchemy(app)
 
+
+
+
+
+
 # Create Model
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +41,29 @@ class UserForm(FlaskForm):
 	email = StringField("Email", validators=[DataRequired()])
 	submit = SubmitField("Submit")
 
+# Update Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+	form = UserForm()
+	name_to_update = Users.query.get_or_404(id)
+	if request.method == "POST":
+		name_to_update.name = request.form['name']
+		name_to_update.email = request.form['email']
+		try:
+			db.session.commit()
+			flash("User Updated Successfully!")
+			return render_template("update.html", 
+				form=form,
+				name_to_update = name_to_update)
+		except:
+			flash("Error!  Looks like there was a problem...try again!")
+			return render_template("update.html", 
+				form=form,
+				name_to_update = name_to_update)
+	else:
+		return render_template("update.html", 
+				form=form,
+				name_to_update = name_to_update)
 
 # Create a Form Class
 class NamerForm(FlaskForm):

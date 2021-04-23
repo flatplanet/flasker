ff28f5df6cfc720bcdaa641cf061f55340e3208d
John Elder <johne4196@gmail.com>
2021-04-23 08:54:31 -0700

    delete record

--- hello.py
+++ hello.py
@@ -39,6 +39,28 @@ class Users(db.Model):
 	def __repr__(self):
 		return '<Name %r>' % self.name
 
+@app.route('/delete/<int:id>')
+def delete(id):
+	user_to_delete = Users.query.get_or_404(id)
+	name = None
+	form = UserForm()
+
+	try:
+		db.session.delete(user_to_delete)
+		db.session.commit()
+		flash("User Deleted Successfully!!")
+
+		our_users = Users.query.order_by(Users.date_added)
+		return render_template("add_user.html", 
+		form=form,
+		name=name,
+		our_users=our_users)
+
+	except:
+		flash("Whoops! There was a problem deleting user, try again...")
+		return render_template("add_user.html", 
+		form=form, name=name,our_users=our_users)
+
 # Create a Form Class
 class UserForm(FlaskForm):
 	name = StringField("Name", validators=[DataRequired()])
@@ -69,7 +91,8 @@ def update(id):
 	else:
 		return render_template("update.html", 
 				form=form,
-				name_to_update = name_to_update)
+				name_to_update = name_to_update,
+				id = id)
 
 # Create a Form Class
 class NamerForm(FlaskForm):

a33963e60bc756d020f6b74eaf5fa027a516dd18
John Elder <johne4196@gmail.com>
2021-05-07 09:23:55 -0700

    password hashing

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 from datetime import datetime 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 
 # Create a Flask Instance
@@ -34,6 +34,19 @@ class Users(db.Model):
 	email = db.Column(db.String(120), nullable=False, unique=True)
 	favorite_color = db.Column(db.String(120))
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+	# Do some password stuff!
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttibuteError('password is not a readable attribute!')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 	# Create A String
 	def __repr__(self):

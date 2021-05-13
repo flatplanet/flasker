fdaca806c90333def0d5162fe3a99e816ec52ee3
John Elder <johne4196@gmail.com>
2021-05-13 13:56:54 -0700

    test hashed passwords vs plaintext

--- hello.py
+++ hello.py
@@ -109,6 +109,11 @@ def update(id):
 				name_to_update = name_to_update,
 				id = id)
 
+class PasswordForm(FlaskForm):
+	email = StringField("What's Your Email", validators=[DataRequired()])
+	password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
 # Create a Form Class
 class NamerForm(FlaskForm):
 	name = StringField("What's Your Name", validators=[DataRequired()])
@@ -180,7 +185,7 @@ def add_user():
 		form.name.data = ''
 		form.email.data = ''
 		form.favorite_color.data = ''
-		form.password_hash = ''
+		form.password_hash.data = ''
 
 		flash("User Added Successfully!")
 	our_users = Users.query.order_by(Users.date_added)
@@ -218,6 +223,38 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+# Create Password Test Page
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+	email = None
+	password = None
+	pw_to_check = None
+	passed = None
+	form = PasswordForm()
+
+
+	# Validate Form
+	if form.validate_on_submit():
+		email = form.email.data
+		password = form.password_hash.data
+		# Clear the form
+		form.email.data = ''
+		form.password_hash.data = ''
+
+		# Lookup User By Email Address
+		pw_to_check = Users.query.filter_by(email=email).first()
+		
+		# Check Hashed Password
+		passed = check_password_hash(pw_to_check.password_hash, password)
+
+	return render_template("test_pw.html", 
+		email = email,
+		password = password,
+		pw_to_check = pw_to_check,
+		passed = passed,
+		form = form)
+
 
 # Create Name Page
 @app.route('/name', methods=['GET', 'POST'])

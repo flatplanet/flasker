13c9547919232ce5d3bcb6d2a165cac1716834ba
John Elder <johne4196@gmail.com>
2021-09-16 08:58:56 -0700

    update correct user

--- hello.py
+++ hello.py
@@ -222,6 +222,7 @@ def delete(id):
 
 # Update Database Record
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
 	form = UserForm()
 	name_to_update = Users.query.get_or_404(id)

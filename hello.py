9f933a2d2ef57e5c96fb936b3f308c5244df2590
John Elder <johne4196@gmail.com>
2021-12-16 10:52:24 -0800

    about author

--- hello.py
+++ hello.py
@@ -110,6 +110,7 @@ def dashboard():
 		name_to_update.email = request.form['email']
 		name_to_update.favorite_color = request.form['favorite_color']
 		name_to_update.username = request.form['username']
+		name_to_update.about_author = request.form['about_author']
 		try:
 			db.session.commit()
 			flash("User Updated Successfully!")
@@ -440,6 +441,7 @@ class Users(db.Model, UserMixin):
 	name = db.Column(db.String(200), nullable=False)
 	email = db.Column(db.String(120), nullable=False, unique=True)
 	favorite_color = db.Column(db.String(120))
+	about_author = db.Column(db.Text(500), nullable=True)
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
 	# Do some password stuff!
 	password_hash = db.Column(db.String(128))
--- webforms.py
+++ webforms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, Length
 from wtforms.widgets import TextArea
 from flask_ckeditor import CKEditorField
@@ -34,6 +34,7 @@ class UserForm(FlaskForm):
 	username = StringField("Username", validators=[DataRequired()])
 	email = StringField("Email", validators=[DataRequired()])
 	favorite_color = StringField("Favorite Color")
+	about_author = TextAreaField("About Author")
 	password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
 	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
 	submit = SubmitField("Submit")
